dc48c254707571c0c97c01f52446a928701fc147
Marco Mat <marcomatildo@gmail.com>
2019-09-02 23:48:36 +0800

    install dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -49494,8 +49494,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! H:\laravel\marcogram\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! H:\laravel\marcogram\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\project\marcogram\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\project\marcogram\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
